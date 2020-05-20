8a23fc2d529ff54d83536a2374b236721758062b
Martin Cornel <mrtncornel@gmail.com>
2020-05-20 08:46:43 +0300

    Setup update to reduce y/n prompts and live test bug fixes

--- code/setup.py
+++ code/setup.py
@@ -5,22 +5,23 @@ def prep_ubuntu():
     # UBUNTU PREPPING
     apt_packages = [
         'python3.7',
-        '-y python3-pip',
+        'python3-pip',
         'build-essential unixodbc-dev',
+        'virtualenv'
     ]
 
     os.system('sudo apt update && sudo apt upgrade -y')
     for package in apt_packages:
-        os.system('sudo apt install %s' % package)
+        os.system('sudo apt install -y %s' % package)
     return True
 
 
 def prep_env():
     commands = [
-        'pip install virtualenv',
-        'virtualenv -p python3 ../../venv',
-        '. ../../etl_venv/bin/activate',
-        'pip install -r requirements.txt'
+        'pip3 install virtualenv',
+        'virtualenv -p python3 ../venv',
+        'source ../venv/bin/activate',
+        'pip install -r code/requirements.txt'
     ]
     for command in commands:
         os.system(command)
