841ee9ad1aac23343b0fd8be6babcb7311bfe41d
Martin Cornel <mrtncornel@gmail.com>
2020-07-29 10:17:54 +0300

    Mysql support completed.

--- code/copy_etl.py
+++ code/copy_etl.py
@@ -1,10 +1,8 @@
 import os
 
 import mysql.connector as mysql_con
-
 from etlhelper import DbParams, execute, connect, copy_rows, get_rows
 from settings import *
-
 
 SHOST = S_HOST
 SPORT = S_PORT
@@ -53,31 +51,52 @@ def copy_src_to_dest():
     dest_conn = get_destination_connection()
     print('Connected to destination')
 
-    # with connect(SOURCE_DB_PARAM, "S_PG_PASSWORD") as src_conn:
-    #     with connect(DESTINATION_DB_PARAMS, "D_PG_PASSWORD") as dest_conn:
     s_tables = []
     if DCLEAR:
         execute(delete_sql, dest_conn)
         print('Cleared destination')
 
+    if engine == ENGINES['mysql']:
+        mysql_copy_src_to_dest(src_conn, s_tables, dest_conn)
+    else:
+        if S_TABLES == '__all__':
+            tables_query = get_tables_query()
+            rows = get_rows(tables_query, src_conn)
+            for row in rows:
+                s_tables.append(row.tablename)
+        else:
+            s_tables = S_TABLES.split(",")
+
+        for S_DB_TABLE in s_tables:
+            select_sql = table_select_query(S_DB_TABLE, src_conn)
+            insert_sql = table_insert_query(D_TABLE)
+            print('Copying data from %s' % S_DB_TABLE)
+            copy_rows(select_sql, src_conn, insert_sql, dest_conn)
+            create_view(S_DB_TABLE, dest_conn)
+
+    refresh_mat_views(dest_conn)
+
+
+def mysql_copy_src_to_dest(src_conn, s_tables, dest_conn):
     if S_TABLES == '__all__':
         tables_query = get_tables_query()
-        rows = get_rows(tables_query, src_conn)
+        rows = get_rows_func(tables_query, src_conn)
         for row in rows:
-            s_tables.append(row.tablename)
-
+            s_tables.append(row[0])
     else:
         s_tables = S_TABLES.split(",")
 
     for S_DB_TABLE in s_tables:
         select_sql = table_select_query(S_DB_TABLE, src_conn)
-        insert_sql = table_insert_query(D_TABLE)
         print('Copying data from %s' % S_DB_TABLE)
-        copy_rows(select_sql, src_conn, insert_sql, dest_conn)
-        create_view(S_DB_TABLE, src_conn, dest_conn)
+        cursor = src_conn.cursor()
+        cursor.execute(select_sql)
+        rows = cursor.fetchall()
+        for row in rows:
+            insert_sql = "INSERT INTO {0} (table_name, fields) VALUES ('{1}', '{2}')".format(D_TABLE, row[0], row[1])
+            execute(insert_sql, dest_conn)
 
-    # if engine == ENGINES['postgres']:
-    refresh_mat_views(dest_conn)
+        create_view(S_DB_TABLE, dest_conn)
 
 
 def get_source_connection():
@@ -95,19 +114,12 @@ def get_rows_func(query, conn):
         cursor = conn.cursor()
         cursor.execute(query)
         rows = cursor.fetchall()
-        res = []
-        for row in rows:
-            res.append(Row(tablename=row[0]))
-
-
         return rows
     rows = get_rows(query, conn)
-    print(rows)
     return rows
 
 
 def table_delete_query(table):
-    # if engine == ENGINES['postgres']:
     return "DELETE FROM {0} where true".format(table)
 
 
@@ -126,13 +138,11 @@ def table_select_query(table, conn):
     elif engine == ENGINES['mysql']:
         columns_query = "SELECT COLUMN_NAME as col_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'{0}'".format(
             table)
-
-        # field_col_pair = "'id', id, 'firstName', first_name, 'lastName', last_name"
         field_col_pair = ""
-        rows = get_rows(columns_query, conn)
+        rows = get_rows_func(columns_query, conn)
 
         for row in rows:
-            field = row.col_name
+            field = row[0]
             field_col_pair += "'{0}', {0}, ".format(field)
 
         field_col_pair = field_col_pair[:-2]
@@ -143,29 +153,24 @@ def table_select_query(table, conn):
 
 
 def table_insert_query(table):
-    # if engine == ENGINES['postgres']: # destination always postgres
     return "INSERT INTO {0} (table_name, fields) VALUES (%s, %s)".format(table)
 
 
 def view_count_query(view):
-    # if engine == ENGINES['postgres']:
     return "select count(*) from pg_catalog.pg_views where schemaname = 'public' and viewname = '%s';" % view
     # return None
 
 
 def sample_rows_query(view):
-    # if engine == ENGINES['postgres']:
     return "select distinct on (1) table_name , fields as fields_json from {1} where table_name = '{0}' order by table_name ; ".format(
         view, D_TABLE)
 
 
 def view_create_query(table, fields, view):
-    # if engine == ENGINES['postgres']:
     return "create view {0} as select {1} from {2} where table_name = '{0}';".format(view, fields, table)
-    # return None
 
 
-def create_view(view_name, src_conn=None, dest_conn=None):
+def create_view(view_name, dest_conn=None):
     """
     Checks if view exists before getting a sample json and creating a view based on the table_name
         :param view_name: table to sample from...also name of destination view to be created
@@ -179,6 +184,7 @@ def create_view(view_name, src_conn=None, dest_conn=None):
         sample_rows = sample_rows_query(view_name)
         rows = get_rows(sample_rows, dest_conn)
         if len(rows) > 0:
+            print(rows[0])
             sample_json = rows[0].fields_json['f1']
             fields = ""
             for k, v in sample_json.items():
@@ -210,10 +216,3 @@ def refresh_mat_views(dest_conn):
 
 
 copy_src_to_dest()
-
-
-
-[('account_periods',), ('accounting_period_balances',), ('actual_agent_businesses',), ('africas_talking_callbacks',), ('agent_classes',), ('agents',), ('alerts',), ('approval_roles',), ('approvals',), ('audits',), ('bank_agreements',), ('bank_branches',), ('bank_deposits',), ('banking_instruments',), ('banking_schedules',), ('banks',), ('black_listed_clients',), ('borrowing_instruments',), ('borrowing_schedules',), ('bounced_payments',), ('budget_targets',), ('budgets',), ('business_rules',), ('cheque_statuses',), ('cheques',), ('client_obligation_statuses',), ('client_obligations',), ('clients',), ('communication_recipients',), ('communication_set_ups',), ('contact_people',), ('crb_scores',), ('crbs',), ('credit_note_usages',), ('credit_notes',), ('dashboard_item_roles',), ('dashboard_items',), ('dashboard_trends',), ('data_migrations',), ('debit_notes',), ('direct_debits',), ('disbursement_schedule_ipf_loans',), ('disbursement_schedules',), ('discount_matrices',), ('env_files',), ('ext_bank_accounts',), ('gl_accounts',), ('gl_entries',), ('incentive_setups',), ('incentives',), ('incoming_cheques',), ('insurer_agreements',), ('insurers',), ('integration_wallet_balances',), ('ipf_discounts',), ('ipf_first_payments',), ('ipf_loan_applications',), ('ipf_loan_approval_limits',), ('ipf_loan_gradings',), ('ipf_loan_statuses',), ('ipf_product_types',), ('ipf_products',), ('jobs',), ('lender_agreements',), ('lenders',), ('loan_agreements',), ('loan_gradings',), ('loans',), ('maker_checker_comments',), ('maker_checker_roles',), ('maker_checkers',), ('manual_journals',), ('manual_matchings',), ('metropol_crbs',), ('migrations',), ('mpesa_call_backs',), ('mpesa_calls',), ('mpesa_payments',), ('out_going_cheques',), ('parameters',), ('password_resets',), ('password_securities',), ('pay_bill_balances',), ('payment_voucher_payments',), ('payment_vouchers',), ('priviledges',), ('product_interest_rates',), ('proratas',), ('public_holiday_years',), ('public_holidays',), ('quotations',), ('rcl_bank_accounts',), ('rcl_obligations',), ('rcl_outputs',), ('recommendations',), ('refunds',), ('replacements',), ('report_roles',), ('report_setups',), ('reports',), ('role_priviledges',), ('roles',), ('sla_actual_averages',), ('slas',), ('sms',), ('staged_approval_comments',), ('staged_approvals',), ('staged_settings',), ('subsidiary_accounts',), ('system_emails',), ('trans_unions',), ('transactions',), ('tutorials',), ('user_incentive_per_ipfs',), ('users',)]
-
-
-[Row(tablename='django_migrations'), Row(tablename='django_content_type'), Row(tablename='qed_suppliersectionscore'), Row(tablename='auth_group_permissions'), Row(tablename='auth_group'), Row(tablename='auth_user_groups'), Row(tablename='auth_permission'), Row(tablename='auth_user_user_permissions'), Row(tablename='django_admin_log'), Row(tablename='django_pesapal_transaction'), Row(tablename='qed_buyerprivilege'), Row(tablename='qed_buyerroleprivilege'), Row(tablename='qed_categorygroup'), Row(tablename='qed_contractdocument'), Row(tablename='qed_contractsectioncategorytype'), Row(tablename='qed_qedprivilege'), Row(tablename='qed_qedroleprivilege'), Row(tablename='qed_qualityassurance'), Row(tablename='oauth2_provider_accesstoken'), Row(tablename='qed_rfqitem'), Row(tablename='oauth2_provider_application'), Row(tablename='qed_setting'), Row(tablename='oauth2_provider_grant'), Row(tablename='oauth2_provider_refreshtoken'), Row(tablename='qed_country'), Row(tablename='qed_currency'), Row(tablename='qed_mpesacallbacks'), Row(tablename='qed_mpesacalls'), Row(tablename='qed_mpesapayment'), Row(tablename='qed_paybillballance'), Row(tablename='qed_pesapalcall'), Row(tablename='qed_pesapalcallback'), Row(tablename='qed_pesapalpayment'), Row(tablename='qed_supplierprequalscore'), Row(tablename='qed_supplierrfqtotal'), Row(tablename='qed_suppliertasksubmission'), Row(tablename='qed_prequalification'), Row(tablename='qed_requestforquotation'), Row(tablename='qed_useraudit'), Row(tablename='qed_tender'), Row(tablename='qed_suppliertenderscore'), Row(tablename='qed_rfqitemresponse'), Row(tablename='qed_requestforquotationinvitee'), Row(tablename='qed_regretletter'), Row(tablename='qed_questionreuse'), Row(tablename='qed_section'), Row(tablename='qed_qualityassuranceresponse'), Row(tablename='qed_qualityassurancequestion'), Row(tablename='qed_qedrole'), Row(tablename='qed_qed'), Row(tablename='qed_prequalificationscore'), Row(tablename='qed_supplierresponse'), Row(tablename='qed_otherresponse'), Row(tablename='qed_question'), Row(tablename='qed_markingscheme'), Row(tablename='qed_sourcingactivity'), Row(tablename='qed_duediligencesupplier'), Row(tablename='qed_duediligencequestion'), Row(tablename='auth_user'), Row(tablename='qed_duediligence'), Row(tablename='qed_contractsection'), Row(tablename='qed_contractcontractsection'), Row(tablename='qed_contract'), Row(tablename='qed_companysupplier'), Row(tablename='qed_categorysupplierpayment'), Row(tablename='qed_payment'), Row(tablename='qed_supplierprofile'), Row(tablename='qed_categoryorder'), Row(tablename='qed_categorytype'), Row(tablename='qed_job'), Row(tablename='qed_buyerrole'), Row(tablename='qed_company'), Row(tablename='qed_buyer'), Row(tablename='qed_category'), Row(tablename='qed_supplier'), Row(tablename='qed_awardletter'), Row(tablename='django_session'), Row(tablename='django_site')]
